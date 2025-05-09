3bc75bec8c7c5af76a478d9b97b9f2bbdd2e2039
Laura Viviana Ramirez Borda <lvramirezb@udistrital.edu.co>
2025-05-09 15:49:47 -0500

    v0.3

--- programa.js
+++ programa.js
@@ -4,3 +4,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([4.627992952025564, -74.06591250553709]).addTo(map);
