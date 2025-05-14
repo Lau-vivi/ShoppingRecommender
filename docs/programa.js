f2f27a386f6b93e78d467daf6a38f23f460b79b6
Laura Viviana Ramirez Borda <lvramirezb@udistrital.edu.co>
2025-05-14 15:32:13 -0500

    v0.9

--- docs/programa.js
+++ docs/programa.js
@@ -5,4 +5,37 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([4.627992952025564, -74.06591250553709]).addTo(map);
+var marker = L.marker([4.627992952025564, -74.06591250553709]);
+marker.addTo(map);
+
+// Abrir un archivo GeoJSON
+// Javascript se ejecuta de tal manera que si una linea
+// de código se demora. se pasa a la siguiente sin esperar.
+// Esto puede generar problemas cuando se requiere secuencialidad 
+
+async function cargarPuntos(){
+    
+    var miArchivo= await fetch("microondas.geojson");
+    //convertir el contenido a json
+
+    var datos= await miArchivo.json();
+
+    //Obtener el arreglo de la llave features que es un conjunto
+    //de objetos tipo feature
+    //let listaFeatures= datos["feature"];
+
+    //Obtener la geometria del primer elemento
+    
+
+    for (let i = 0; i < 10; i++) {
+
+        let misCoordenadas= listaFeatures[i]["geometry"]["coordinates"];
+        var miMarcador= L.marker(miscoordenadas);
+        miMarcador.addTo(map);
+    
+   
+}
+
+}
+cargarPuntos();
+//console.log(datos["features"][i]);
