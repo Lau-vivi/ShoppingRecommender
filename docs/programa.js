68db36115b6463bdfa8c7861df3920e1dc302a73
Laura Viviana Ramirez Borda <lvramirezb@udistrital.edu.co>
2025-05-16 15:32:17 -0500

    v0.12

--- docs/programa.js
+++ docs/programa.js
@@ -9,30 +9,46 @@ var marker = L.marker([4.627992952025564, -74.06591250553709]);
 marker.addTo(map);
 
 // Abrir un archivo GeoJSON
-// Javascript se ejecuta de tal manera que si una linea
-// de código se demora. se pasa a la siguiente sin esperar.
-// Esto puede generar problemas cuando se requiere secuencialidad 
-
 async function CargarPuntos() {
 
     var miArchivo = await fetch("microondas.geojson");
     // Convertir el archivo a JSON
-    
     var datos = await miArchivo.json();
 
     // Obtener el arreglo de la llave features
     let listaFeatures = datos["features"];
 
-    for (let i = 0; i < 10; i++) {
+    // Recorrer los elementos del archivo GeoJSON
+    for (let i = 0; i < listaFeatures.length; i++) {
         console.log(datos["features"][i]);
 
         // Obtener la geometría del elemento
         let misCoordenadas = listaFeatures[i]["geometry"]["coordinates"];
 
+        // Obtener las propiedades del elemento
+        let propiedades = listaFeatures[i]["properties"];
+
+        // Crear el marcador
         var miMarcador = L.marker(misCoordenadas);
+         // Crear el contenido del popup con las propiedades que mencionaste
+        const contenidoPopup = `
+            <strong>Modelo:</strong> ${propiedades.Modelo}<br>
+            <strong>Capacidad:</strong> ${propiedades.Capacidad} L<br>
+            <strong>Precio:</strong> $${propiedades.Precio}<br>
+            <strong>Precio con Descuento:</strong> $${propiedades.PrecioDescuento}<br>
+            <strong>Potencia:</strong> ${propiedades.Potencia} W<br>
+            <strong>Voltaje:</strong> ${propiedades.Voltaje} V<br>
+            <strong>Alto:</strong> ${propiedades.Alto} cm<br>
+            <strong>Ancho:</strong> ${propiedades.Ancho} cm<br>
+            <strong>Profundidad:</strong> ${propiedades.Profundidad} cm
+        `;
+        
+        // Asignar el popup al marcador
+        miMarcador.bindPopup(contenidoPopup);
+
+        // Agregar el marcador al mapa
         miMarcador.addTo(map);
     }
-
-}
+};
 
 CargarPuntos();
